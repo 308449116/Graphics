7c7cb1c3ab90294f885018fbc2d44615820f884a
‘hougongxiang’ <308449116@qq.com>
2024-01-03 17:01:19 +0800

    update

---
+++ UndoCmd/itemcreatecmd.cpp
@@ -0,0 +1,33 @@
+#include "itemcreatecmd.h"
+#include "viewgraphics.h"
+#include "graphicsitem.h"
+
+ItemCreateCmd::ItemCreateCmd(GraphicsItemType type, ViewGraphics *view, QUndoCommand *parent)
+    : QUndoCommand(parent), m_type(type), m_view(view), m_item(nullptr)
+{
+//    QString str("Create %1");
+//    this->setText(str.arg(view->getItemDisplayName(type)));
+}
+
+ItemCreateCmd::~ItemCreateCmd()
+{
+
+}
+
+void ItemCreateCmd::undo()
+{
+    if (m_item) {
+        m_view->removeItem(m_item);
+    }
+}
+
+void ItemCreateCmd::redo()
+{
+    if (m_item) {
+        m_view->addItem(m_item);
+    } else {
+        m_item = m_view->createItemByType(m_type);
+        QString str("Create %1");
+        this->setText(str.arg(m_item->getItemName()));
+    }
+}
---
+++ UndoCmd/itemcreatecmd.h
@@ -0,0 +1,27 @@
+#ifndef ITEMCREATECMD_H
+#define ITEMCREATECMD_H
+
+#include <QUndoCommand>
+
+#include "common.h"
+#include "viewgraphics.h"
+
+class ItemCreateCmd : public QUndoCommand
+{
+public:
+    explicit ItemCreateCmd(GraphicsItemType type,
+                           ViewGraphics *view, QUndoCommand *parent = nullptr);
+
+    ~ItemCreateCmd();
+
+    void undo() override;
+
+    void redo() override;
+
+private:
+    GraphicsItemType m_type;
+    ViewGraphics *m_view;
+    QSharedPointer<GraphicsItem> m_item;
+};
+
+#endif
